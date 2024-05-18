f1497310e2b714c6050d0d8ea4994d44ecc4f2f7
MissterMan <putrayogi4545@gmail.com>
2024-05-18 13:59:48 +0700

    Adding project categories crud

---
+++ src/controllers/projectCategoriesController.ts
@@ -0,0 +1,148 @@
+import { Request, Response } from "express";
+import { response } from "../utils/response";
+import { v4 as uuidv4 } from "uuid";
+import ProjectCategory from "../models/projectCategoryModel";
+import {
+  createProjectCategories,
+  deleteProjectCategories,
+  getProjectCategory,
+  getProjectCategoryById,
+  updateProjectCategories,
+} from "../repositories/projectCategoryRepository";
+
+export const getProjectCategoriesController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const categories = await getProjectCategory();
+    return response(200, categories, "Get all categories data", res);
+  } catch (error) {
+    console.error(error);
+    return response(
+      500,
+      "Data error",
+      "An error occured while retrieving data",
+      res
+    );
+  }
+};
+
+export const getProjectCategoriesByIdController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const uuid: string = req.params.uuid;
+    const categories = await getProjectCategoryById(uuid);
+    if (categories === null) {
+      return response(404, categories, "Categories not found", res);
+    } else {
+      return response(200, categories, "Get categories by ID", res);
+    }
+  } catch (error) {
+    console.error(error);
+    return response(
+      500,
+      "Data error",
+      "An error occured while retrieving data",
+      res
+    );
+  }
+};
+
+export const createProjectCategoriesController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const data: ProjectCategory = req.body;
+    if (!data.name) {
+      return response(400, "Data error", "All data are required", res);
+    }
+
+    const name: string = data.name;
+    const uuid: string = uuidv4();
+    const created_at: Date = new Date();
+    const updated_at: Date = created_at;
+
+    const categoriesData: ProjectCategory = {
+      name,
+      uuid,
+      created_at,
+      updated_at,
+    };
+
+    const result = await createProjectCategories(categoriesData);
+    return response(201, result, "Categories created", res);
+  } catch (error) {
+    console.error(error);
+    return response(
+      500,
+      "Data error",
+      "An error occured while creating data",
+      res
+    );
+  }
+};
+
+export const updateProjectCategoriesController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const uuid: string = req.params.uuid;
+    const data: ProjectCategory = req.body;
+
+    if (!data.name) {
+      return response(400, "Data error", "All data are required", res);
+    }
+
+    // Set updated data
+    const name: string = data.name;
+    const updated_at: Date = new Date();
+
+    // Assign updated data
+    const updatedCategories: ProjectCategory = {
+      uuid,
+      name,
+      updated_at,
+    };
+
+    const result = await updateProjectCategories(uuid, updatedCategories);
+    if (result === null) {
+      return response(404, "Data not found", `Categories not found`, res);
+    }
+    return response(200, result, `Categories updated`, res);
+  } catch (error) {
+    console.error(error);
+    return response(
+      500,
+      "Data error",
+      "An error occured while creating data",
+      res
+    );
+  }
+};
+
+export const deleteProjectCategoriesController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const uuid: string = req.params.uuid;
+    await deleteProjectCategories(uuid);
+    return response(200, "Data deleted", "Categories removed", res);
+  } catch (error: any) {
+    console.error(error);
+    if (error.message === "No rows were affected.") {
+      return response(404, "Data not found", `Categories not found`, res);
+    }
+    return response(
+      500,
+      "Data error",
+      "An error occured while creating data",
+      res
+    );
+  }
+};
--- src/repositories/projectCategoryRepository.ts
+++ src/repositories/projectCategoryRepository.ts
@@ -1,1 +1,91 @@
 import pool from "../configs/database";
+import ProjectCategory from "../models/projectCategoryModel";
+
+export const getProjectCategory = async (): Promise<any[]> => {
+  const query = "SELECT * FROM pipeline.project_categories";
+  try {
+    const result = await pool.query(query);
+    return result.rows;
+  } catch (error) {
+    console.log("Error when query all user:", error);
+    throw error;
+  }
+};
+
+export const getProjectCategoryById = async (
+  uuid: string
+): Promise<any[] | null> => {
+  const query = `SELECT * FROM pipeline.project_categories WHERE uuid = $1`;
+  try {
+    const result = await pool.query(query, [uuid]);
+    if (result.rows.length === 0) {
+      return null;
+    }
+    return result.rows;
+  } catch (error) {
+    console.error("Error when query categories by ID:", error);
+    throw error;
+  }
+};
+
+export const createProjectCategories = async (
+  data: ProjectCategory
+): Promise<any[]> => {
+  const query = `INSERT INTO pipeline.project_categories 
+  (name, uuid, created_at, updated_at) 
+  VALUES ($1, $2, $3, $4) 
+  RETURNING *;`;
+
+  const values = [data.name, data.uuid, data.created_at, data.updated_at];
+
+  try {
+    const result = await pool.query(query, values);
+    if (result.rowCount === 0) {
+      throw new Error("No rows were affected");
+    }
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error when inserting new categories:", error);
+    throw error;
+  }
+};
+
+export const updateProjectCategories = async (
+  uuid: string,
+  data: ProjectCategory
+): Promise<any[] | null> => {
+  const query = `
+  UPDATE pipeline.project_categories
+  SET name = $1, updated_at = $2
+  WHERE uuid = $3
+  RETURNING *;
+  `;
+
+  const values = [data.name, data.updated_at, uuid];
+
+  try {
+    const result = await pool.query(query, values);
+    if (result.rows.length === 0) {
+      return null;
+    } else if (result.rowCount === 0) {
+      throw new Error("No rows were affected.");
+    }
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error updating project categories:", error);
+    throw error;
+  }
+};
+
+export const deleteProjectCategories = async (uuid: string) => {
+  const query = `DELETE FROM pipeline.pipeline_users WHERE uuid = $1`;
+  try {
+    const result = await pool.query(query, [uuid]);
+    if (result.rowCount === 0) {
+      throw new Error("No rows were affected.");
+    }
+  } catch (error) {
+    console.error("Error deleting project categories:", error);
+    throw error;
+  }
+};
