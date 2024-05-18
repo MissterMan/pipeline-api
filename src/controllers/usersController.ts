1a3a8a631ac3bd92c82a207a7462603bd5fef70e
MissterMan <putrayogi4545@gmail.com>
2024-05-18 13:58:54 +0700

    Adding email dan password validation when creating and updating user

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -70,12 +70,64 @@ export const createUserController = async (req: Request, res: Response) => {
       return response(400, "Data error", "All data are required", res);
     }
 
+    // Password Validation
+    const passwordValidate = data.password.trim();
+    if (passwordValidate.length < 8) {
+      return response(
+        400,
+        "Password error",
+        "Password must be at least 8 characters long",
+        res
+      );
+    }
+    if (!/\d/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one number",
+        res
+      );
+    }
+    if (!/[A-Z]/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one uppercase letter",
+        res
+      );
+    }
+    if (!/[a-z]/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one lowercase letter",
+        res
+      );
+    }
+    if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one special character",
+        res
+      );
+    }
+    // End of password validation
+
+    // Email Validation
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValidate = data.email.trim();
+    if (!emailRegex.test(emailValidate)) {
+      return response(400, "Email error", "Email format is invalid", res);
+    }
+    // End of email validation
+
     // Create additional data
     const name: string = data.name;
-    const email: string = data.email;
+    const email: string = emailValidate;
     const role: string = data.role;
     const birthdate: string = new Date(data.birthdate).toJSON().split("T")[0];
-    const password: string = await hashPassword(data.password);
+    const password: string = await hashPassword(passwordValidate);
     const uuid: string = uuidv4();
     const created_at: Date = new Date();
     const updated_at: Date = created_at;
@@ -114,13 +166,67 @@ export const updateUserController = async (req: Request, res: Response) => {
       return response(400, "Data error", "All data are required", res);
     }
 
+    // Password Validation
+    const passwordValidate = data.password.trim();
+    if (passwordValidate.length < 8) {
+      return response(
+        400,
+        "Password error",
+        "Password must be at least 8 characters long",
+        res
+      );
+    }
+    if (!/\d/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one number",
+        res
+      );
+    }
+    if (!/[A-Z]/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one uppercase letter",
+        res
+      );
+    }
+    if (!/[a-z]/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one lowercase letter",
+        res
+      );
+    }
+    if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(passwordValidate)) {
+      return response(
+        400,
+        "Password error",
+        "Password must contain at least one special character",
+        res
+      );
+    }
+    // End of password validation
+
+    // Email Validation
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValidate = data.email.trim();
+    if (!emailRegex.test(emailValidate)) {
+      return response(400, "Email error", "Email format is invalid", res);
+    }
+    // End of email validation
+
+    // Set updated data
     const name: string = data.name;
-    const email: string = data.email;
+    const email: string = emailValidate;
     const role: string = data.role;
     const birthdate: string = new Date(data.birthdate).toJSON().split("T")[0];
-    const password: string = await hashPassword(data.password);
+    const password: string = await hashPassword(passwordValidate);
     const updated_at: Date = new Date();
 
+    // Assign updated data
     const updatedUser: User = {
       uuid,
       name,
@@ -130,6 +236,7 @@ export const updateUserController = async (req: Request, res: Response) => {
       password,
       updated_at,
     };
+
     const result = await updateUser(uuid, updatedUser);
     if (result === null) {
       return response(404, "Data not found", `User ${uuid} not found`, res);
